b2786baa7e9378b648e8b7581d9a23138e94497f
Jaifan <jiku1213@gmail.com>
2022-12-19 18:13:38 +0000

    Test done

--- test/unit/TodoList.test.js
+++ test/unit/TodoList.test.js
@@ -19,10 +19,63 @@ const {assert,expect} = require("chai")
             await todoList.addTask("Zakaria")
             expect(parseInt(await todoList.getTaskListLength(deployer.address)),2)
             const {taskName, taskStatus} = await todoList.getTaskWithIndex(0,deployer.address)
-            //assert.equal(await todoList.getTaskWithIndex(0,deployer.address))
             assert.equal(taskName,'Jaifan')
             assert.equal(taskStatus,0)
         }) 
+        it("task name length should be more than or equal three character",async()=>{
+            await expect(todoList.addTask("Ji")).to.be.revertedWith("TodoList__TaskNameMinimumThree")
+            await expect(todoList.addTask("")).to.be.revertedWith("TodoList__TaskNameMinimumThree")
+        })
+        it("an event excuted right after taskn  register",async()=>{
+            await expect(todoList.addTask("Mr Github")).to.emit(todoList,"AddTask")  
+        })
+    })
+
+    describe("editTaskStatus",function(){
+        it("task index should be valid",async()=>{
+            await expect(todoList.editTaskStatus(0)).to.be.revertedWith("TodoList__TaskIndexInvalid") 
+        })
+        it("task status should change",async()=>{
+            await todoList.addTask("Jaifan")
+            await todoList.addTask("Zakaria")
+            await todoList.editTaskStatus(1)
+            const {taskStatus} = await todoList.getTaskWithIndex(1,deployer.address)
+            assert(taskStatus,1)      
+        })
+    })
+
+    describe("editTaskName",function(){
+        it("task index should be valid",async()=>{
+            await todoList.addTask("Hello")
+            await expect(todoList.editTaskName(1,"Jaifan")).to.be.revertedWith("TodoList__TaskIndexInvalid")
+        })
+        it("edit name should be more than or equal three chacter",async()=>{
+            await todoList.addTask("Hello")
+            await expect(todoList.editTaskName(0,"J")).to.be.revertedWith("TodoList__TaskNameMinimumThree")
+        })
+        it("edit name successfully",async()=>{
+            await todoList.addTask("Hello")
+            await todoList.editTaskName(0,"Hello Jaifan")
+            const {taskName} = await todoList.getTaskWithIndex(0,deployer.address)
+            assert(taskName,"Hello Jaifan")
+        })
+    })
+    
+    describe("taskDelete",function(){
+        it("task index should be valid",async()=>{
+            await todoList.addTask("Hello")
+            await expect(todoList.taskDelete(1)).to.be.revertedWith("TodoList__TaskIndexInvalid")
+        })
+        it("delete the task successfully",async()=>{
+            await todoList.addTask("Hello")
+            await todoList.addTask("Hello2")
+            await todoList.addTask("Hello3")
+            await todoList.addTask("Hello4")
+            await todoList.taskDelete(1)
+            await todoList.taskDelete(0)
+            const length = parseInt(await todoList.getTaskListLength(deployer.address))
+            assert(2,length)
+        })
     })
 })
 
